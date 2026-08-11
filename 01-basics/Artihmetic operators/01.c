207e3866e92eda50797d5431a9662450a3dce1c0
Deepanshu Jeena <deepanshujeenaa@gmail.com>
2026-08-12 00:12:48 +0530

    Add files via upload

---
+++ 01-basics/Artihmetic operators/01.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(){
+    // arthmetic operators 
+
+    int x = 2;
+    int y = 3;
+    int z = 0;
+
+
+    //z= x+y;
+    //z= x-y;
+    //z= x*y;
+    //z =x / y;
+    //z = x % 2
+    //x++;
+    //x--;
+    // argumented assignment operators 
+    //x+=2; // x = x+2
+    //x-=2; // x =x-2
+    // x*=2;
+     x/=2;
+
+
+
+
+    printf("%d",x);
+
+
+    //printf("%f",z);
+
+    return 0;
+
+}
